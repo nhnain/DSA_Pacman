9db126fa381362fd79f5726408a276292443f459
NGUYEN HUYNH NGAN ANH <nhnanh2508@gmail.com>
2024-12-07 00:31:50 +0700

    update ghost finding direction algorithm

---
+++ ghost.js
@@ -0,0 +1,264 @@
+class Ghost {
+    constructor(
+        x,
+        y,
+        width,
+        height,
+        speed,
+        imageX,
+        imageY,
+        imageWidth,
+        imageHeight,
+        range
+    ) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        // const DIRECTION_RIGHT = 4;
+        // const DIRECTION_UP = 3;
+        // const DIRECTION_LEFT = 2;
+        // const DIRECTION_BOTTOM = 1;
+        this.direction = DIRECTION_RIGHT;
+        this.imageX = imageX;
+        this.imageY = imageY;
+        this.imageHeight = imageHeight;
+        this.imageWidth = imageWidth;
+        this.range = range;
+        this.randomTargetIndex = parseInt(Math.random() * 4);
+        this.target = randomTargetsForGhosts[this.randomTargetIndex];
+        setInterval(() => {
+            this.changeRandomDirection();
+        }, 10000);
+    }
+
+    isInRange() {
+        let xDistance = Math.abs(pacman.getMapX() - this.getMapX());
+        let yDistance = Math.abs(pacman.getMapY() - this.getMapY());
+        if (
+            Math.sqrt(xDistance * xDistance + yDistance * yDistance) <=
+            this.range
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    changeRandomDirection() {
+        let addition = 1;
+        this.randomTargetIndex += addition;
+        this.randomTargetIndex = this.randomTargetIndex % 4;
+    }
+
+    moveProcess() {
+        if (this.isInRange()) {
+            this.target = pacman;
+        } else {
+            this.target = randomTargetsForGhosts[this.randomTargetIndex];
+        }
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            return;
+        }
+    }
+
+    moveBackwards() {
+        switch (this.direction) {
+            case 4: // Right
+                this.x -= this.speed;
+                break;
+            case 3: // Up
+                this.y += this.speed;
+                break;
+            case 2: // Left
+                this.x += this.speed;
+                break;
+            case 1: // Bottom
+                this.y -= this.speed;
+                break;
+        }
+    }
+
+    moveForwards() {
+        switch (this.direction) {
+            case 4: // Right
+                this.x += this.speed;
+                break;
+            case 3: // Up
+                this.y -= this.speed;
+                break;
+            case 2: // Left
+                this.x -= this.speed;
+                break;
+            case 1: // Bottom
+                this.y += this.speed;
+                break;
+        }
+    }
+
+    checkCollisions() {
+        let isCollided = false;
+        if (
+            map[parseInt(this.y / oneBlockSize)][
+                parseInt(this.x / oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize + 0.9999)][
+                parseInt(this.x / oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize)][
+                parseInt(this.x / oneBlockSize + 0.9999)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize + 0.9999)][
+                parseInt(this.x / oneBlockSize + 0.9999)
+            ] == 1
+        ) {
+            isCollided = true;
+        }
+        return isCollided;
+    }
+
+    changeDirectionIfPossible() {
+        let tempDirection = this.direction;
+        this.direction = this.calculateNewDirection(
+            map,
+            parseInt(this.target.x / oneBlockSize),
+            parseInt(this.target.y / oneBlockSize)
+        );
+        if (typeof this.direction == "undefined") {
+            this.direction = tempDirection;
+            return;
+        }
+        if (
+            this.getMapY() != this.getMapYRightSide() &&
+            (this.direction == DIRECTION_LEFT ||
+                this.direction == DIRECTION_RIGHT)
+        ) {
+            this.direction = DIRECTION_UP;
+        }
+        if (
+            this.getMapX() != this.getMapXRightSide() &&
+            this.direction == DIRECTION_UP
+        ) {
+            this.direction = DIRECTION_LEFT;
+        }
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            this.direction = tempDirection;
+        } else {
+            this.moveBackwards();
+        }
+        console.log(this.direction);
+    }
+
+    calculateNewDirection(map, destX, destY) {
+        const start = { x: this.getMapX(), y: this.getMapY(), moves: [] };
+        const end = { x: destX, y: destY };
+        const queue = [start];
+        const visited = new Set();
+        visited.add(`${start.x},${start.y}`);
+        const directions = [
+            { x: 0, y: -1, move: DIRECTION_UP },    // UP
+            { x: 1, y: 0, move: DIRECTION_RIGHT },  // RIGHT
+            { x: 0, y: 1, move: DIRECTION_BOTTOM },   // DOWN
+            { x: -1, y: 0, move: DIRECTION_LEFT }   // LEFT
+        ];
+    
+        while (queue.length > 0) {
+            const current = queue.shift();
+    
+            if (current.x == end.x && current.y == end.y) {
+                return current.moves[0]; // Return the first move in the path
+            }
+    
+            for (const dir of directions) {
+                const neighborX = current.x + dir.x;
+                const neighborY = current.y + dir.y;
+    
+                if (
+                    neighborX >= 0 && neighborX < map[0].length &&
+                    neighborY >= 0 && neighborY < map.length &&
+                    map[neighborY][neighborX] != 1 &&
+                    !visited.has(`${neighborX},${neighborY}`)
+                ) {
+                    const neighbor = {
+                        x: neighborX,
+                        y: neighborY,
+                        moves: current.moves.concat(dir.move),
+                        parent: current
+                    };
+                    queue.push(neighbor);
+                    visited.add(`${neighborX},${neighborY}`);
+                }
+            }
+        }
+    
+        return 1; // Default direction if no path is found
+    }
+
+    getMapX() {
+        let mapX = parseInt(this.x / oneBlockSize);
+        return mapX;
+    }
+
+    getMapY() {
+        let mapY = parseInt(this.y / oneBlockSize);
+        return mapY;
+    }
+
+    getMapXRightSide() {
+        let mapX = parseInt((this.x * 0.99 + oneBlockSize) / oneBlockSize);
+        return mapX;
+    }
+
+    getMapYRightSide() {
+        let mapY = parseInt((this.y * 0.99 + oneBlockSize) / oneBlockSize);
+        return mapY;
+    }
+
+    changeAnimation() {
+        this.currentFrame =
+            this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
+    }
+
+    draw() {
+        canvasContext.save();
+        canvasContext.drawImage(
+            ghostFrames,
+            this.imageX,
+            this.imageY,
+            this.imageWidth,
+            this.imageHeight,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+        canvasContext.restore();
+        canvasContext.beginPath();
+        canvasContext.strokeStyle = "red";
+        canvasContext.arc(
+            this.x + oneBlockSize / 2,
+            this.y + oneBlockSize / 2,
+            this.range * oneBlockSize,
+            0,
+            2 * Math.PI
+        );
+        canvasContext.stroke();
+    }
+}
+
+let updateGhosts = () => {
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].moveProcess();
+    }
+};
+
+let drawGhosts = () => {
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].draw();
+    }
+};
